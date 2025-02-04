a8f59212bc8940462d588e27370505a50a56a2bc
vlshugaiphnmn <vlad_shugai@phenomenon-studio.com>
2025-02-04 13:40:32 +0200

    fix: build

--- app/about/team.tsx
+++ app/about/team.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, EffectFade } from "swiper/modules";
 import "swiper/css";
